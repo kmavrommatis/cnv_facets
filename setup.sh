a687c670a6da54fce91320874e83d13ca5cfeaf2
mavrommk_BMS <konstantinos.mavrommatis@bms.com>
2025-08-04 12:16:53 +0200

    Fixed typos

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 #
 # Copyright (C) 2018-2019 University of Glasgow
 #
-# Author: Dario Beraldi <dario.beraldi@glasgow.ac.uk>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
